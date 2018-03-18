a38cd4799025b37be81d2a41229d62d9c4153d72
minh <minh@ubuntu.ubuntu-domain>
2018-03-18 04:20:23 -0400

    added approx_plane.C

--- bitflip.c
+++ bitflip.c
@@ -2,6 +2,19 @@
 //#define TEST
 #define LIMIT 16
 
+int ipow(int base, int exp)
+{
+    int result = 1;
+    while (exp)
+    {
+        if (exp & 1)
+            result *= base;
+        exp >>= 1;
+        base *= base;
+    }
+
+    return result;
+}
 int generate_bit_position()
 {
         int num;
@@ -51,10 +64,22 @@ int bitflip_int_at_pos (int num, int pos)
         return num ^ (1<<pos);
 }
 
+int limitprecision_int (int num, int precision) // saturated max value for signed int
+{
+		int limit = ipow(2,precision-1);
+		if(num > limit-1)
+			return limit -1;
+		if (num < (0-limit))
+			return 0 - limit;
+		return num;
+}
+
 #ifdef TEST
 int main()
 {
         double num = 0.8;
         printf("%lf\n",bitflip_float(num));
+        int num2 = 15;
+        printf ("%d %d \n", limitprecision_int(num2,4), limitprecision_int(num2,5));
 }
 #endif
--- build_Montecarlo.sh
+++
@@ -1,1 +0,0 @@
-gcc -o approx_Montecarlo  approx_Montecarlo.c bitflip.c Random.c Stopwatch.c -lm -lmpfr
